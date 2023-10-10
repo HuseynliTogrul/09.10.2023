5ec253e60bfba4fee5f231293f0e032f6c09699c
HuseynliTogrul <toghrulih@code.edu.az>
2023-10-11 01:48:50 +0400

    metod

---
+++ metod/metod/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] nums = { 1, 2, 3 };
+int num = 1;
+
+bool status = false;
+bool Find(int[] nums, int num)
+{
+    for (int i = 0; i < nums.Length; i++){
+        if (num == nums[i])
+        {
+            status = true;
+        }
+    }
+    return status;
+}
+Console.WriteLine(Find(nums, num));
